70eb98cb55d77af7393fc8126e8384ca446a5e37
JasonQorbin <Jason.Pieterse@Outlook.com>
2024-06-14 07:48:10 +0200

    Hello world route on Express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require( 'express' );
+const app = express();
+const bodyParser = require( 'body-parser' );
+const PORT = process.env.PORT || 8000;
+
+app.use(bodyParser.urlencoded( {  extended : true } ));
+app.use(bodyParser.json());
+
+app.get('/', (request, response) => {
+    response.status(200).send("Hello World!!!").end();
+});
+
+app.listen(PORT, () => {console.log(`Server now listening on port ${PORT}`)});
+
