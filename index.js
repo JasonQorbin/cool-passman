fda2ef8afa99ecd34e84aab397420048e26f480b
JasonQorbin <Jason.Pieterse@Outlook.com>
2024-06-15 15:46:25 +0200

    Install helmet

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require( 'express' );
 const app = express();
 const bodyParser = require( 'body-parser' );
+const helmet = require('helmet');
 const PORT = process.env.PORT || 8000;
+
+app.use(helmet());
 
 app.use(bodyParser.urlencoded( {  extended : true } ));
 app.use(bodyParser.json());
