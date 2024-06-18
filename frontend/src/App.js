10f2e5a32f9085560cedc3b79c7f5169fc0f9534
JasonQorbin <Jason.Pieterse@Outlook.com>
2024-06-18 22:34:50 +0200

    Install and add React routing with BrowserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import './App.css';
-import PageHeader from './components/PageHeader';
-import SideBar from './components/SideBar';
+import "./App.css";
+import { Routes, Route, Outlet } from "react-router-dom";
+import PageHeader from "./components/PageHeader";
+import SideBar from "./components/SideBar";
 import LoginScreen from "./components/LoginScreen";
 import RegisterScreen from "./components/RegisterScreen";
 import HomeScreen from "./components/HomeScreen";
@@ -8,13 +9,27 @@ import HomeScreen from "./components/HomeScreen";
 function App() {
   return (
     <div className="App">
-        <PageHeader />
-        <div className="content-row">
-            <SideBar />
-            <HomeScreen />
-        </div>
+        <Routes>
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
+            <Route path="/" element={<MainPageLayout />}>
+                <Route path="/home" element={<HomeScreen />} />
+            </Route>
+        </Routes>
     </div>
   );
 }
 
 export default App;
+
+function MainPageLayout() {
+    return (
+        <div>
+            <PageHeader />
+            <div className="content-row">
+                <SideBar />
+                <Outlet />
+            </div>
+        </div>
+    )
+}
