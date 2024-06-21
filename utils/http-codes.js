a2703795da5b6b49d950015e5462fb6b9fac81ad
JasonQorbin <Jason.Pieterse@Outlook.com>
2024-06-22 00:07:44 +0200

    Doc comments on the http-codes file

--- utils/http-codes.js
+++ utils/http-codes.js
@@ -1,20 +1,33 @@
 //Status codes
 
 const SUCCESS                = 200;
-const CREATED                = 201; //A new resource was created.
-const BAD_REQUEST            = 400; //Something is wrong with the request (missing or incorrect fields). Fix and try again.
-const UNAUTHORISED           = 401; //The requestor could not be authenticated. Auth header may be wrong or token may be expired. Fix and try again.
-
-const FORBIDDEN              = 403; //The requester was positively authenticated but is not authorized to access the resource.
-                                    //A retry will not change the outcome.
+/** A new resource was created. */
+const CREATED                = 201;
+/** Something is wrong with the request (missing or incorrect fields). Fix and try again. */
+const BAD_REQUEST            = 400;
+/** 
+ * The requestor could not be authenticated. 
+ * The auth header may be wrong or token may be expired. The client is expected to fix the problem and retry. 
+ */
+const UNAUTHORISED           = 401;
+/** 
+ * The requester was positively authenticated but is not authorized to access the resource.
+ * The client should bother retrying because the outcome should be the same.
+ */
+const FORBIDDEN              = 403;                                   //A retry will not change the outcome.
 /** The requested resource could not be found. */
 const NOT_FOUND              = 404;
-const CONFLICT               = 409; //The requested change conflicts with the current state of the server.
-const GONE                   = 410; //The requested resource has been pernamently deleted.
-const UNSUPPORTED_MEDIA_TYPE = 415; //The request's Content-Type is wrong
-const TOO_MANY_REQUESTS      = 429; //Rate limit exceeded
+/** The requested change conflicts with the current state of the server. */
+const CONFLICT               = 409;
+//The requested resource has been pernamently deleted. Retrying will not change the outcome */
+const GONE                   = 410; 
+/** The request's Content-Type is wrong */
+const UNSUPPORTED_MEDIA_TYPE = 415;
+/** //Rate limit exceeded. */
+const TOO_MANY_REQUESTS      = 429; 
 const INTERNAL_SERVER_ERROR  = 500;
 const NOT_IMPLEMENTED        = 501;
+/** The requested end-point/service is not available. This state is expected to be temporary */
 const SERVICE_UNAVAILABLE    = 503;
 
 
