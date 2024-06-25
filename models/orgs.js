e5198a0611f20500b23180b3e1dabacbdc11b676
JasonQorbin <Jason.Pieterse@Outlook.com>
2024-06-25 19:45:20 +0200

    Change depts and repos to use sub-docs in db.
    The departments and repos are not defined with their own schema
    and nested inside the org schema as sub-documents as per the
    Mongoose documentation. Previously they were sub-paths which are
    harder to query because I couldn't progressively pull out a sub doc.

--- models/orgs.js
+++ models/orgs.js
@@ -1,6 +1,44 @@
 const mongoose = require('mongoose');
 
 /**
+ * Schema for each credential that will sit in the department repos.
+ */
+const credentialSchema = mongoose.Schema(
+    {
+        name : {
+            type: String,
+            trim : true,
+            required: true,
+        },
+        url : {
+            type: String,
+            trim : true,
+            required: true,
+        },
+        username : {
+            type: String,
+            trim : true,
+        },
+        password : String
+    }
+);
+
+/**
+ * Schema for the department documents that will sit inside the orgs.
+ */
+const deptSchema = mongoose.Schema(
+    {
+        name : {
+            type: String,
+            trim : true,
+            required: true,
+        },
+
+        repo : [credentialSchema]
+    }
+);
+
+/**
  * Model for an organisation. Contains an array of sub-documents for the departments and within that is
  * The repo for that department in the form of a list of credentials as documents.
  */
@@ -12,29 +50,7 @@ const orgSchema = mongoose.Schema(
             required : true,
             unique : true
         },
-        departments : [
-            {
-                name : {
-                    type : String,
-                    trim: true,
-                    required : true
-                },
-
-                repo : [
-                    {
-                        resource_name : {
-                            type : String,
-                            required : true,
-                            trim : true,
-                        },
-                        URL : String,
-                        username : String,
-                        password : String,
-                    }
-                ]
-
-            }
-        ]
+        departments : [deptSchema]
     }
 );
 
