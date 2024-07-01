f02d24d9ee1f3c9fefcf4dd03a08bc054a03fec1
JasonQorbin <Jason.Pieterse@Outlook.com>
2024-07-02 00:48:42 +0200

    Add authorization middleware to the repo routes

--- routes/repoRoutes.js
+++ routes/repoRoutes.js
@@ -4,11 +4,12 @@
 const express = require('express');
 const repoRouter = express.Router();
 const OrgController = require('../controllers/OrgController');
+const { adminsOnly, adminsAndTheSameUserOnly, authorisedManagersOnly, authorisedUsersOnly, allRegisteredUsers } = require('../middleware/authorization');
 
-repoRouter.get('/', OrgController.getRepos);
-repoRouter.post('/:orgID/:deptID', OrgController.addCredential);
-repoRouter.put('/:orgID/:deptID/:credID', OrgController.replaceCredential);
-repoRouter.delete('/:orgID/:deptID/:credID', OrgController.deleteCredential);
+repoRouter.get   ('/',                       allRegisteredUsers,     OrgController.getRepos);
+repoRouter.post  ('/:orgID/:deptID',         authorisedUsersOnly,    OrgController.addCredential);
+repoRouter.put   ('/:orgID/:deptID/:credID', authorisedManagersOnly, OrgController.replaceCredential);
+repoRouter.delete('/:orgID/:deptID/:credID', authorisedManagersOnly, OrgController.deleteCredential);
 
 
 module.exports = repoRouter;
